f5f70348bd3587a5e1593140d96414364e5883d9
cdavis1809 <c3imaging@outlook.com>
2024-02-26 18:53:19 -0500

    added basic javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let recipeFormElement = document.querySelector("#recipe-form");
+  recipeFormElement.addEventListener("submit", getRecipe);
+});
 
+function getRecipe(event) {
+  event.preventDefault();
+  new Typewriter("#recipe", {
+    strings: ["Preparing your recipe..."],
+    autoStart: true,
+    delay: NamedNodeMap,
+    cursor: "",
+  });
+}
