893e4f3f461129cffff65fccc8c544c931f6201b
cdavis1809 <c3imaging@outlook.com>
2024-02-26 19:54:03 -0500

    included API call and edited prompt

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ document.addEventListener("DOMContentLoaded", function () {
   recipeFormElement.addEventListener("submit", getRecipe);
 });
 
-function getRecipe(event) {
-  event.preventDefault();
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: ["Preparing your recipe..."],
+    strings: response.data.answer,
     autoStart: true,
     delay: NamedNodeMap,
     cursor: "",
   });
+}
+
+function getRecipe(event) {
+  event.preventDefault();
+  let input = document.querySelector("#user-instructions");
+  let apiKey = "406035333ab93t5b036b8515eob03bf5";
+  let prompt = `Please find a recipe that is based off ${input.value} cuisine. `;
+  let context =
+    "You are a world class chef specializing in quick and delicious meals. Generate a meal that will be ready in less than an hour and only requires one pan/pot. Please provide ingredients in a bulleted list with one ingredient per line followed by numbered steps each on a separate line. Please sign 'SheCodes AI' in a <strong></strong> element at the end of the recipe.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
